feeb9cff3e002fae7818fa5ce879256c35038450
Ton4ee <166876423+Ton4ee@users.noreply.github.com>
2024-05-25 20:38:01 +0200

    Initial commit

--- SILab2.java
+++ SILab2.java
@@ -48,46 +48,46 @@ class Item {
 
 
 public class SILab2 {
-    public static boolean checkCart(List<Item> allItems, int payment){
-        if (allItems == null){
-            throw new RuntimeException("allItems list can't be null!");
+    public static boolean checkCart(List<Item> allItems, int payment){ // 1
+        if (allItems == null){ //2
+            throw new RuntimeException("allItems list can't be null!"); //3
         }
 
-        float sum = 0;
+        float sum = 0; //4
 
-        for (int i = 0; i < allItems.size(); i++){
-            Item item = allItems.get(i);
-            if (item.getName() == null || item.getName().length() == 0){
-                item.setName("unknown");
+        for (int i = 0; i < allItems.size(); i++){ //5
+            Item item = allItems.get(i); //6
+            if (item.getName() == null || item.getName().length() == 0){ //7
+                item.setName("unknown"); //8
             }
-            if (item.getBarcode() != null){
-                String allowed = "0123456789";
-                char chars[] = item.getBarcode().toCharArray();
-                for (int j = 0; j < item.getBarcode().length(); j++){
-                    char c = item.getBarcode().charAt(j);
-                    if (allowed.indexOf(c) == -1){
-                        throw new RuntimeException("Invalid character in item barcode!");
+            if (item.getBarcode() != null){ //9
+                String allowed = "0123456789"; //10
+                char chars[] = item.getBarcode().toCharArray(); //11
+                for (int j = 0; j < item.getBarcode().length(); j++){ //12
+                    char c = item.getBarcode().charAt(j); //13
+                    if (allowed.indexOf(c) == -1){ //14
+                        throw new RuntimeException("Invalid character in item barcode!"); //15
                     }
+                } //16
+                if (item.getDiscount() > 0){ //17
+                    sum += item.getPrice()*item.getDiscount(); //18
                 }
-                if (item.getDiscount() > 0){
-                    sum += item.getPrice()*item.getDiscount();
+                else { //19
+                    sum += item.getPrice(); //20
                 }
-                else {
-                    sum += item.getPrice();
-                }
+            } //21
+            else { //22
+                throw new RuntimeException("No barcode!"); //23
             }
-            else {
-                throw new RuntimeException("No barcode!");
+            if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){ //24
+                sum -= 30; //25
             }
-            if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){
-                sum -= 30;
-            }
+        } //26
+        if (sum <= payment){ //27
+            return true; //28
         }
-        if (sum <= payment){
-            return true;
+        else { //29
+            return false; //30
         }
-        else {
-            return false;
-        }
-    }
+    } //31
 }
